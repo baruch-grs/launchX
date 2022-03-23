dc6857f3287df663c43baa80059aee6ed001ee6c
BaruchGuerra <dolce_dmn@hotmail.com>
2022-03-23 02:03:47 -0600

    added more styles and added error management

--- Frontend/Pokedex/src/pokedex.js
+++ Frontend/Pokedex/src/pokedex.js
@@ -2,10 +2,18 @@ const getPokemonInfo = async () => {
   const pokeNameInput = document.getElementById('pokeName').value.toLowerCase();
   const url = `https://pokeapi.co/api/v2/pokemon/${pokeNameInput}`;
   try {
-    let response = await fetch(url);
-    response = await response.json();
-    pokeImage(response.sprites.front_default);
-    showPokeInfo(pokeInfo(response));
+    const response = await fetch(url);
+    console.log(response);
+    let informationWithFormat = await response.json();
+    console.log(response);
+
+    if (pokeNameInput.length <= 0 || response.status !== 200) {
+      let errorUrlImage = 'https://www.publicdomainpictures.net/pictures/280000/nahled/not-found-image-15383864787lu.jpg';
+      pokeImage(errorUrlImage);
+    }
+
+    pokeImage(informationWithFormat.sprites.front_default);
+    showPokeInfo(pokeInfo(informationWithFormat));
   } catch (error) {
     throw new Error('ERROR', error);
   }
@@ -14,11 +22,10 @@ const showPokeInfo = (data) => {
   let element = document.getElementById('pokeInfo');
   element.innerHTML = `
     <ul>
-      <li> ${data.height} </li>
-      <li> ${data.weight} </li>
-      <li> ${data.types} </li>
+      <li> Height: ${data.height} </li>
+      <li> Weight: ${data.weight} </li>
+      <li> Types: ${data.types} </li>
     </ul>
-
   `;
 }
 
