f8d5563c1bfded0de07f8227a70513e1ba164788
Baruch Guerra <dolce_dmn@hotmail.com>
2022-04-19 22:34:07 -0500

    added semicolons to endlines, added a console.table to visualize better all the information

--- Backend/weekly_mission_1/example1/main.js
+++ Backend/weekly_mission_1/example1/main.js
@@ -5,16 +5,16 @@ myCar.make = 'Ford'; // Guardando un valor dentro del objeto creado
 myCar.model = 'Mustang';
 myCar.year = 1969;
 
-console.log(myCar) // Imprimiendo objeto
+console.log(myCar); // Imprimiendo objeto
 
 // 2. Declaración de un objeto con variables locales y públicas
 
 const myModule = (() => {
 
   // Variables de contexto local
-  const privateFoo = "Soy un valor privado, solo me usan dentro de este objeto"
-  const privateBar = [1, 2, 3, 4]
-  const baz = "Soy un valor que va a ser expuesto"
+  const privateFoo = "Soy un valor privado, solo me usan dentro de este objeto";
+  const privateBar = [1, 2, 3, 4];
+  const baz = "Soy un valor que va a ser expuesto";
 
   // Variable para guardar las variables locales
   const exported = {
@@ -24,7 +24,7 @@ const myModule = (() => {
   }
 
   // Exposición de variables locales
-  return exported
-})()
-
-console.log(myModule)
+  return exported;
+})();
+console.table(myModule);
+console.log(myModule);
--- Backend/weekly_mission_1/example2/main.js
+++ Backend/weekly_mission_1/example2/main.js
@@ -1,6 +1,8 @@
 // node main.js
 
-const logger = require('./logger')
+const logger = require('./logger');
 
-logger.info('This is an informational message')
-logger.verbose('This is a verbose message')
+logger.info('This is an informational message');
+logger.info('Un mensaje diferente, saludos')
+logger.verbose('This is a verbose message');
+logger.verbose('un verbose diferente, saludos')
