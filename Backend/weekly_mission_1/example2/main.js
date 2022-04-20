cf7d99195ee69d70ace73d4ea4963ffedf419544
Baruch Guerra <dolce_dmn@hotmail.com>
2022-04-19 22:40:17 -0500

    added another folder with another example to export objects in JS

--- Backend/weekly_mission_1/example2/main.js
+++ Backend/weekly_mission_1/example2/main.js
@@ -3,6 +3,6 @@
 const logger = require('./logger');
 
 logger.info('This is an informational message');
-logger.info('Un mensaje diferente, saludos')
+logger.info('Un mensaje diferente, saludos');
 logger.verbose('This is a verbose message');
-logger.verbose('un verbose diferente, saludos')
+logger.verbose('un verbose diferente, saludos');
