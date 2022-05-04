214627fa8b9c6465e68e0d4bc0c500366a30d139
Baruch Guerra <baruch.gr@hotmail.com>
2022-05-04 14:00:37 -0500

    Added more test to practice TDD, added start script to package json

--- Backend/weekly_mission_3/spiderverse/app/Spiderman.js
+++ Backend/weekly_mission_3/spiderverse/app/Spiderman.js
@@ -6,6 +6,22 @@ class Spiderman {
     this.movies = movies;
     this.actor = actor;
   }
+  get getName() {
+    return this.name;
+  }
+  get getActor() {
+    return this.actor;
+  }
+  get getMovies() {
+    return this.movies;
+  }
+
+  get getAge() {
+    return this.age;
+  }
+  getInfo() {
+    return `Hey, I'm ${this.actor} from Marvel studio`;
+  }
 }
 
 module.exports = Spiderman;
--- Backend/weekly_mission_3/spiderverse/test/Spiderman.test.js
+++ Backend/weekly_mission_3/spiderverse/test/Spiderman.test.js
@@ -2,10 +2,18 @@ const Spiderman = require('../app/Spiderman');
 describe("Tests for the Spiderman class", () => {
   test('Case 1: Get a Spiderman object  ', () => {
     const andrew = new Spiderman('Spiderman Sony', 31, 'Sony', 2, 'Andrew Garfield');
+    const holland = new Spiderman('Spiderman Marvel', 25, 'Marvel', 5, 'Tom Holland');
+
+    expect(andrew.getMovies).toBe(2);
     expect(andrew.name).toBe('Spiderman Sony');
     expect(andrew.age).toBe(31);
     expect(andrew.actor).toBe('Andrew Garfield');
     expect(andrew.movies).toBe(2);
     expect(andrew.studio).toBe('Sony');
+    expect(holland.getAge).toBe(25);
+    expect(holland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio");
+    expect(holland.getActor).toBe('Tom Holland');
+    expect(holland.getName).toBe('Spiderman Marvel');
+
   });
 });
