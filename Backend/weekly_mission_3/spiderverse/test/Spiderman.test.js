d88493447ac88587b8e02d179898f1c8642aa99d
Baruch Guerra <baruch.gr@hotmail.com>
2022-05-03 23:05:52 -0500

    Added the second exercise, started with TDD concepts

---
+++ Backend/weekly_mission_3/spiderverse/test/Spiderman.test.js
@@ -0,0 +1,5 @@
+describe("Tests for the Spiderman class", () => {
+  test('Case 1: Get a Spiderman object  ', () => {
+    const andrew = new Spiderman('Spiderman Sony', 31, "Andrew Garfield", 2)
+  });
+});
