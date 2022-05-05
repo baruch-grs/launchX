74888acd1ce5209b7aac6b7165f51ea431b8e527
Baruch Guerra <baruch.gr@hotmail.com>
2022-05-04 19:36:38 -0500

    Added getters and setters to the user model, added the unit testing to getters and setters, added comments to the unit tests

--- Backend/weekly_mission_3/Twitter/test/User.test.js
+++ Backend/weekly_mission_3/Twitter/test/User.test.js
@@ -2,13 +2,31 @@ const User = require('../app/models/User');
 
 describe("Test Suite for User model", () => {
   test('Case 1: Attributes', () => {
-    const user = new User(1, 'ezpk', 'Baruch Guerra', 'My bio', 'dateCreated', 'lastUpdated');
+    const user = new User(1, 'ezpk', 'Baruch Guerra', 'My bio');
 
+    // Setters tests
+    user.setUsername = 'Elhyoz';
+    expect(user.username).toBe('Elhyoz');
+
+    user.setBio = 'New bio';
+    expect(user.bio).toBe('New bio');
+
+    // Return values before the change to test it
+    user.setBio = 'My bio';
+    user.setUsername = 'ezpk';
+
+    // Getters tests
+    expect(user.getBio).toBe('My bio');
+    expect(user.getDateCreated).not.toBeUndefined();
+    expect(user.getUsername).toBe('ezpk');
+    expect(user.getLastUpdated).not.toBeUndefined();
+
+    // Attributes tests
     expect(user.id).toBe(1);
     expect(user.username).toBe('ezpk');
     expect(user.name).toBe('Baruch Guerra');
     expect(user.bio).toBe('My bio');
-    expect(user.dateCreated).toBe('dateCreated');
-    expect(user.lastUpdated).toBe('lastUpdated');
+    expect(user.dateCreated).not.toBeUndefined();
+    expect(user.lastUpdated).not.toBeUndefined();
   });
 });
