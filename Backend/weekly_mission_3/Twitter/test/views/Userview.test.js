f3abcca6cfb89b7c7e19fe3fe17b81cc5ea4cc3e
Baruch Guerra <baruch.gr@hotmail.com>
2022-05-05 23:42:30 -0500

    Added the tests of the second use case and the code to the userview class

--- Backend/weekly_mission_3/Twitter/test/views/Userview.test.js
+++ Backend/weekly_mission_3/Twitter/test/views/Userview.test.js
@@ -4,7 +4,17 @@ describe('Test suite for UserView class', () => {
   test('Case 1: Return an error object when a null is sended', () => {
     const payload = null;
     const result = UserView.createUser(payload);
-
     expect(result.error).toMatch(/payload no existe/);
+  });
+
+  test('Case 2. Return an error object when try to create a new user with payload with invalid properties', () => {
+    const payload = {
+      username: null,
+      name: 54,
+      id: 'my id',
+    };
+
+    const result = UserView.createUser(payload);
+    expect(result.error).toMatch(/necesitan tener valores válidos/);
   });
 });
