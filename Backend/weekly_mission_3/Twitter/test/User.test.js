947957971d030ba664f9a1384afc4a1cc37b1019
Baruch Guerra <baruch.gr@hotmail.com>
2022-05-05 21:36:12 -0500

    Added new test for dates in user

--- Backend/weekly_mission_3/Twitter/test/User.test.js
+++ Backend/weekly_mission_3/Twitter/test/User.test.js
@@ -9,6 +9,10 @@ describe("Test Suite for User model", () => {
     expect(user.username).toBe('ezpk');
     expect(user.name).toBe('Baruch Guerra');
     expect(user.bio).toBe('My bio');
+  });
+
+  test('Adding dates in user attributes', () => {
+    const user = new User(1, 'ezpk', 'Baruch Guerra', 'My bio');
     expect(user.dateCreated).not.toBeUndefined();
     expect(user.lastUpdated).not.toBeUndefined();
   });
